69ea6e0f186d3bcc281a626389c5c154e505d53b
AUDLINK\sridhar <sridhar@audlink.net>
2019-07-29 16:11:30 +0530

    done the changes

--- app/src/main/java/com/testing/newapp/MainActivity.java
+++ app/src/main/java/com/testing/newapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.testing.newapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.testing.newapp.fragmentUI.PaymentChargingFragment;
+import com.testing.newapp.fragmentUI.PaymentInsertCard;
+
 public class MainActivity extends AppCompatActivity {
+    private static MainActivity mainActivity;
+
+    public static MainActivity getInstance() {
+        return mainActivity;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainActivity = this;
+//        loadFragmentUI("PaymentCharging");
+        loadFragmentUI("PaymentInsert");
+    }
+
+    public void loadFragmentUI(String type) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        switch (type) {
+            case "PaymentCharging":
+                Fragment paymentChargingFragment = new PaymentChargingFragment();
+                transaction.replace(R.id.frameLoader, paymentChargingFragment);
+                transaction.commit();
+                break;
+            case "PaymentInsert":
+                Fragment paymentInsertFragment = new PaymentInsertCard();
+                transaction.replace(R.id.frameLoader, paymentInsertFragment);
+                transaction.commit();
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/testing/newapp/fragmentUI/PaymentInsertCard.java
+++ app/src/main/java/com/testing/newapp/fragmentUI/PaymentInsertCard.java
@@ -1,17 +1,52 @@
 package com.testing.newapp.fragmentUI;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.testing.newapp.R;
 
 public class PaymentInsertCard extends Fragment {
+    LinearLayout layViewDetails, layAmountInfoDropDown;
+    boolean isLayAmountInfoDropDownVisibility = false;
+    ImageView imaArrow1,imaArrow2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_payment_insert_card, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        layViewDetails = view.findViewById(R.id.layViewDetails);
+        layAmountInfoDropDown = view.findViewById(R.id.layAmountInfoDropDown);
+        imaArrow1=view.findViewById(R.id.imaArrow1);
+        imaArrow2=view.findViewById(R.id.imaArrow2);
+        layAmountInfoDropDown.setVisibility(View.GONE);
+
+        layViewDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isLayAmountInfoDropDownVisibility) {
+                    layAmountInfoDropDown.setVisibility(View.VISIBLE);
+                    isLayAmountInfoDropDownVisibility = true;
+                    imaArrow1.setRotation(90);
+                    imaArrow2.setRotation(90);
+                } else {
+                    layAmountInfoDropDown.setVisibility(View.GONE);
+                    isLayAmountInfoDropDownVisibility = false;
+                    imaArrow1.setRotation(270);
+                    imaArrow2.setRotation(270);
+                }
+            }
+        });
     }
 }
