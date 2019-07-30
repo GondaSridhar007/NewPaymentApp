1d831b2a85728a2d2240b48c51c8e755f87487c7
Sridhar Gonda <sridhar@audlink.in>
2019-07-30 23:27:49 +0530

    Added the Disconnected screen added

--- app/src/main/java/com/testing/newapp/MainActivity.java
+++ app/src/main/java/com/testing/newapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.testing.newapp.fragmentUI.PaymentChargingFragment;
+import com.testing.newapp.fragmentUI.PaymentDisconnected;
 import com.testing.newapp.fragmentUI.PaymentInsertCard;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 loadFragmentUI("PaymentInsert");
+                loadDisconnected();
+            }
+        }, SPLASH_DELAY);
+    }
+
+    private void loadDisconnected() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadFragmentUI("Disconnected");
             }
         }, SPLASH_DELAY);
     }
@@ -42,6 +53,11 @@ public class MainActivity extends AppCompatActivity {
             case "PaymentInsert":
                 Fragment paymentInsertFragment = new PaymentInsertCard();
                 transaction.replace(R.id.frameLoader, paymentInsertFragment);
+                transaction.commit();
+                break;
+            case "Disconnected":
+                Fragment disconnectedFragment = new PaymentDisconnected();
+                transaction.replace(R.id.frameLoader, disconnectedFragment);
                 transaction.commit();
                 break;
         }
